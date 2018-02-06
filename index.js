042445420c6b0d30bed6a539919f2285ee080980
William Hansen <cade.hansen@gmail.com>
2018-02-05 16:52:06 -0800

    working on miner download and data storage

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const gpuInfo = require('gpu-info');
 gpuInfo().then(function(data) {
-    data.forEach(element => {
-        console.log(element.AdapterCompatibility, element.Name)
-    });
+    // data.forEach(element => {
+    //     console.log(element.AdapterCompatibility, element.Name)
+    // });
+    console.log(data)
 });
+
+const request = require('request');
+const fs = require('fs');
+
+request.get('https://github.com/KlausT/ccminer/releases/download/8.20/ccminer-820-cuda91-x64.zip').pipe(fs.createWriteStream('ccminer-820-cuda91-x64.zip'));
